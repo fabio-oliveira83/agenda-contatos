3082204091fb859853334f16baff3eeb398fb37c
fabio-oliveira83 <fabiooliveira.sjc@hotmail.com>
2024-08-05 11:36:33 -0300

    fixed: mascara de telefonee

--- maskTel.js
+++ maskTel.js
@@ -1,13 +1,27 @@
 
 $(document).ready(function() {
-$('#telefone-contato').mask('(00) 00000-0000');
 
-$('#telefone-contato').on('input', function() {
-    var telefone = $(this).val().replace(/\D/g, ''); // Remove caracteres não numéricos
-    if (telefone.length === 10) {
-      $(this).mask('(00) 0000-0000'); // Máscara para telefone fixo
-    } else if (telefone.length === 11) {
-      $(this).mask('(00) 00000-0000'); // Máscara para celular
+  function aplicarMascara() {
+
+    var telefone = $('#telefone-contato').val().replace(/\D/g, '');
+
+    if (telefone.length > 10) {
+
+      $('#telefone-contato').mask('(00) 00000-0000');
+
+    } else {
+
+
     }
-    });
+
+  }
+
+  $('#telefone-contato').mask('(00) 0000-00009');
+
+  $('#telefone-contato').on('input', function() {
+
+    aplicarMascara();
+
+  });
+
 });
